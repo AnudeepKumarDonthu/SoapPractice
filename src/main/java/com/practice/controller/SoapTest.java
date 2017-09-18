92f0efe3a761a60c573075b9e333d9706a549766
Anudeep Kumar Donthu <anudeepdonthu@gmail.com>
2017-09-18 05:49:30 +0530

    soap practice

---
+++ src/main/java/com/practice/controller/SoapTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.practice.controller;
+
+/**
+ * @author AnudeepKumar
+ *
+ */
+public class SoapTest {
+
+	public String stringValue() {
+		return "Hello Soap Web service";
+	}
+	
+	public int addValues(int a, int b) {
+		return a+b;
+	}
+
+}
